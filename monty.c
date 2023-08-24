dff31c418406e28947c7df2617bdcddbde83c7c2
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-24 08:52:18 +0000

    Made huge changes

--- monty.c
+++ monty.c
@@ -4,67 +4,71 @@ stack_t *head = NULL;
 
 int main(int ac, char **av)
 {
-    FILE *file = NULL;
-    char *buffer = NULL;
-    int line = 1;
-    ssize_t count;
-    size_t size = 0;
-    char **strings = NULL;
+	FILE *file = NULL;
+	char *buffer = NULL;
+	int line = 1;
+	ssize_t count;
+	size_t size = 0;
+	char **strings = NULL;
 
-    if (ac == 2)
-    {
-        file = fopen(av[1], "r");
-        if (file == NULL)
-            error(av[1]);
-        while ((count = getline(&buffer, &size, file)) != -1)
+
+	if (ac == 2)
+	{
+		file = fopen(av[1], "r");
+		if (file == NULL)
+			error(av[1]);
+		while ((count = getline(&buffer, &size, file)) != -1)
         {
-            /* Trim newline character at the end of the line */
-            if (count > 0 && buffer[count - 1] == '\n') {
-                buffer[count - 1] = '\0';
-            }
+			/* Trim newline character at the end of the line */
+			if (count > 0 && buffer[count - 1] == '\n')
+			{
+				buffer[count - 1] = '\0';
+			}
 
-            if (strcmp(buffer, "") == 0) {
-                /* Blank line, skip to next iteration */
-                line++;
-                continue;
-            }
+			if (strcmp(buffer, "") == 0)
+			{
+				/* Blank line, skip to next iteration */
+				line++;
+				continue;
+			}
 
-            strings = _strtok(buffer);
+			strings = _strtok(buffer);
 
-            if (strcmp("pall", strings[0]) == 0)
-                pall();
-            else if (strcmp("push", strings[0]) == 0)
-                push(strings, line);
-            else if (strcmp("pint", strings[0]) == 0)
-                pint(strings, line);
-            else if (strcmp("pop", strings[0]) == 0)
-                pop(line);
-            else if (strcmp("swap", strings[0]) == 0)
-                swap(strings, line);
-            else if (strcmp("add", strings[0]) == 0)
-                add(strings, line);
-            else if (strcmp("nop", strings[0]) == 0)
-                nop();
-            else
-            {
-                printf("L%d: unknown instruction %s\n", line, strings[0]);
-                exit(EXIT_FAILURE);
-            }
+			if (strcmp("pall", strings[0]) == 0)
+				pall();
+			else if (strcmp("push", strings[0]) == 0)
+				push(strings, line);
+			else if (strcmp("pint", strings[0]) == 0)
+				pint(strings, line);
+			else if (strcmp("pop", strings[0]) == 0)
+				pop(line);
+			else if (strcmp("swap", strings[0]) == 0)
+				swap(strings, line);
+			else if (strcmp("add", strings[0]) == 0)
+				add(strings, line);
+			else if (strcmp("nop", strings[0]) == 0)
+				nop();
+			else
+			{
+				printf("L%d: unknown instruction %s\n", line, strings[0]);
+				exit(EXIT_FAILURE);
+			}
+			line++;
+		}
+	}
+	else
+		usage();
 
-            line++;
-        }
-    }
-    else
-        usage();
-    
-    if (file) {
-        fclose(file);
-    }
-    if (buffer) {
-        free(buffer);
-    }
-
-    return 0;
+	if (file)
+	{
+		fclose(file);
+	}
+	if (buffer)
+	{
+		free(buffer);
+	}
+	free_tokens(strings);
+	return (0);
 }
 
 /**
@@ -73,8 +77,8 @@ int main(int ac, char **av)
 
 void usage(void)
 {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
 }
 
 /**
@@ -83,6 +87,6 @@ void usage(void)
  */
 void error(char *av)
 {
-         fprintf(stderr, "Error: Can't open file %s\n", av);
-         exit(EXIT_FAILURE);
+	fprintf(stderr, "Error: Can't open file %s\n", av);
+	exit(EXIT_FAILURE);
 }
