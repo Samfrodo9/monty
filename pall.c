7948cc85b98e59063b187fbe4d9c8c53b94ad5d1
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-24 08:31:15 +0000

    Made huge changes

--- pall.c
+++ pall.c
@@ -15,13 +15,13 @@ int pall(void)
 
 	transverse = head;
 
-	while (transverse->next != NULL)
-		transverse = transverse->next;
+/*	while (transverse->next != NULL) */
+/*		transverse = transverse->next; */
 
 	while (transverse !=  NULL)
 	{
 		printf("%d\n", transverse->n);
-		transverse = transverse->prev;
+		transverse = transverse->next;
 	}
 
 
